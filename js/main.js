39f85172f1bca53e6708c2638315cbf94b452069
Ana Letícia Paiva <analeticia.s.paiva@gmail.com>
2022-05-20 13:37:33 -0300

    Ajustes finais para apresentação
    Foram feitos alguns ajustes necessários no código para melhor entendimento e fluidez das informações como, por exemplo, o ajuste no nome de algumas classes, a melhoria na acessibilidade , melhorias no script.

--- js/main.js
+++ js/main.js
@@ -1,51 +1,49 @@
+//Variáveis - Box de input
 const inputPesquisaUsuario = document.querySelector('#inserir-user');
 const pesquisarBtn = document.querySelector('#pesquisar');
-
+//Variáveis - Box com dados principais do usuário
 const imagemUsuario = document.querySelector('.icon');
 const nomeUsuario = document.querySelector('.nome');
 const usuarioGit = document.querySelector('.user');
 const biografia = document.querySelector('#biografia');
 const dataCriacaoConta = document.querySelector('.data-criacao-conta');
-
-const textoDadosPerfil = document.querySelector('.texto');
+//Variáveis - Box com número do perfil
 const repositorios = document.querySelector('.repositorio');
 const seguidores = document.querySelector('.seguidores');
 const seguindo = document.querySelector('.seguindo');
-
+//Variáveis - Box com links e localização 
 const localizacao = document.querySelector('.localizacao');
 const linkPaginaWeb = document.querySelector('.pagina-web');
 const usernameTwitter = document.querySelector('.twitter');
 const linkEmpresarial = document.querySelector('.empresa');
 
 async function renderiza() {
-
-    const insereNovoUsuario = inputPesquisaUsuario.value;
-
     try {
+        const insereNovoUsuario = inputPesquisaUsuario.value;
         const resposta = await fetch(`https://api.github.com/users/${insereNovoUsuario}`);
+        document.querySelector(".usuarios-box-externo").style.display = 'block';
+        document.querySelector(".sem-resultados").style.display = 'none';
 
         if (resposta.status === 200) {
-            document.querySelector(".usuarios-box-externo").style.display = 'block';
-            document.querySelector(".sem-resultados").style.display = 'none';
             const data = await resposta.json();
             console.log(data);
 
             imagemUsuario.src = `${data.avatar_url}`;
-            nomeUsuario.innerText = `${data.name}`;
-            usuarioGit.innerText = `@${data.login}`;
-            biografia.innerText = (data.bio === null) ? 'Não há bio neste perfil' : `${data.bio}`;
+            nomeUsuario.textContent = (data.name === null) ? 'Usuário sem nome' : `${data.name}`;
+            usuarioGit.textContent = `@${data.login}`;
+            biografia.textContent = (data.bio === null) ? 'Não há bio neste perfil' : `${data.bio}`;
 
             const criadoEm = new Date(data.created_at);
-            const dataFormatada = criadoEm.toLocaleDateString('pt-br', { year: 'numeric', month: 'short', day: 'numeric' });
+            const dataFormatada = criadoEm.toLocaleDateString('pt-br', { day: 'numeric', month: 'short', year: 'numeric' });
             dataCriacaoConta.textContent = `Desde ${dataFormatada}`;
 
-            repositorios.innerText = `${data.public_repos} `;
-            seguidores.innerText = `${data.followers} `;
-            seguindo.innerText = `${data.following} `;
-            localizacao.innerText = (data.location === null) ? 'Não Disponível' : `${data.location} `;
+            repositorios.textContent = `${data.public_repos} `;
+            seguidores.textContent = `${data.followers} `;
+            seguindo.textContent = `${data.following} `;
+            localizacao.textContent = (data.location === null) ? 'Não Disponível' : `${data.location} `;
             linkPaginaWeb.innerHTML = (data.blog === "") ? 'Não Disponível' : `${data.blog} `;
-            usernameTwitter.innerText = (data.twitter_username === null || "") ? 'Não Disponível' : `${data.twitter_username} `;
-            linkEmpresarial.innerText = (data.company === null || "") ? 'Não disponível' : `${data.company} `;
+            usernameTwitter.textContent = (data.twitter_username === null) ? 'Não Disponível' : `${data.twitter_username} `;
+            linkEmpresarial.textContent = (data.company === null) ? 'Não disponível' : `${data.company} `;
         }
         else if (resposta.status === 404) {
             document.querySelector(".usuarios-box-externo").style.display = 'none';
